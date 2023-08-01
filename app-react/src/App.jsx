7088cf2daaeb9d3e0c65de2616d33dc09042afb8
anakaren.guzman.contractor <anakaren.guzman.contractor@bbva.com>
2023-08-01 10:41:57 -0600

    Multiple de 5

---
+++ app-react/src/App.jsx
@@ -0,0 +1,11 @@
+import { Count } from "./components/Counter";
+import React from "react";
+
+
+function App() {
+
+ return(
+    <Count/>
+    )
+}
+export default App
---
+++ app-react/src/components/Multiple.jsx
@@ -0,0 +1,10 @@
+import "../styles/multiple.css"
+
+export function Multiplo({estado}){
+
+    return(
+        <>
+        <h2 id="multiple">{estado}</h2>
+        </>
+    )
+}
